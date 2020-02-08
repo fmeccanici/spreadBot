5eae8766a0d2925e7896d667a46989cb6f490bd3
Floris <fmeccanici@tudelft.net>
2020-02-08 14:33:44 +0100

    invalid price error

--- spreadBot.py
+++ spreadBot.py
@@ -1,22 +1,75 @@
 import ccxt
 import time
     
+class spreadBot():
 
-f = open('data.csv', 'a')
-f.write("bid price, ask price, bid quanitity, ask quantity, spread,\n")
-f.close()
+    def __init__(self, apiKey, secretKey):
+        self.bitmex   = ccxt.bitmex(
+            {
+            'apiKey': apiKey,
+            'secret': secretKey,
+            'timeout': 30000,
+            'enableRateLimit': True,
+            }   
+        )
+        self.symbol = 'BTC/USD'
+        self.balance = {'BTC':0.0, 'USD':0.0}
+
+    def collect_data(self):
+        f = open('data.csv', 'a')
+        f.write("bid price, ask price, bid quantity, ask quantity, spread,\n")
+        f.close()
+        while True:
+            try:
+                # spread = []
+                orderbook = self.bitmex.fetch_order_book(self.symbol)
+                bids = orderbook['bids']
+                asks = orderbook['asks']
+
+                with open('data.csv', 'a') as f:
+                        # spread.append(asks[i][0] - bids[i][0])
+                        # print(asks[i][1])
+                        f.write(str(bids[0][0]) + ", " + str(asks[0][0]) + ", " + str(bids[0][1]) + ", " + str(asks[0][1]) + ", " + str(asks[0][0] - bids[0][0]) + " USD,\n")
+                time.sleep(2)
+            except Exception as e: print(e) 
 
 
-bitmex   = ccxt.bitmex()
-exchange_id = 'bitmex'
-exchange_class = getattr(ccxt, exchange_id)
-exchange = exchange_class({
-    'apiKey': 'UEQtbUhwe2UGg3l8csciE2nE',
-    'secret': '6cd2OBoFEr1_qzJmdlr2uoxAciRceJyGnQ5malr7aodnT4md',
-    'timeout': 30000,
-    'enableRateLimit': True,
-})
+    def run(self):
+        btc_balance = self.bitmex.fetch_balance()['BTC']['free']
+        orderbook = self.bitmex.fetch_order_book(self.symbol)
+        bids = orderbook['bids']
+        asks = orderbook['asks'] 
 
+        sell_price = asks[0][0]
+        buy_price = bids[0][0]
+        sell_quantity = asks[0][1]
+        buy_quantity = bids[0][1]
+
+        spread = sell_price - buy_price
+
+        print(buy_price)
+        # print(buy_quantity)
+        quantity = min(buy_quantity, sell_quantity, btc_balance/2*buy_price, btc_balance/2*sell_price)
+        order_type = 'market'
+        params = {'execInst': 'ParticipateDoNotInitiate'}
+
+        # print(spread)
+        # print(buy_price)
+
+        # if spread > 2.0: 
+        self.bitmex.create_order(self.symbol, order_type, 'buy', int(quantity+0.5), (buy_price), params)
+        self.bitmex.create_order(self.symbol, order_type, 'sell', int(quantity+0.5), (sell_price), params)
+
+if __name__ == "__main__":
+
+    apiKey = 'UEQtbUhwe2UGg3l8csciE2nE'
+    secretKey = '6cd2OBoFEr1_qzJmdlr2uoxAciRceJyGnQ5malr7aodnT4md'
+
+    spread_bot = spreadBot(apiKey, secretKey)
+    while True:
+        try:
+            spread_bot.run()
+        except Exception as e: print(e)
 # hitbtc_markets = hitbtc.load_markets()
 
 # print(hitbtc.id, hitbtc_markets)
@@ -35,18 +88,6 @@ exchange = exchange_class({
 
 # print("spread = " + str(spread))
 
-while True:
-    # spread = []
-    orderbook = bitmex.fetch_order_book('BTC/USD')
-    bids = orderbook['bids']
-    asks = orderbook['asks']
-
-    with open('data.csv', 'a') as f:
-        for i in range(min(len(bids), len(asks))):
-            # spread.append(asks[i][0] - bids[i][0])
-            # print(asks[i][1])
-            f.write(str(bids[i][0]) + ", " + str(asks[i][0]) + ", " + str(bids[i][1]) + ", " + str(asks[i][1]) + ", " + str(asks[i][0] - bids[i][0]) + " USD,\n")
-    time.sleep(1)
 
 # print(huobipro.fetch_trades('LTC/CNY'))
 
